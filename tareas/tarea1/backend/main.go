457f456404e6cb4588260ee5265c43fce58bee8a
Christian Blanco <3617836980101@ingenieria.usac.edu.gt>
2024-01-29 00:59:17 -0600

    [ADD]FUNCIONAL

---
+++ tareas/tarea1/backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/rs/cors"
+)
+
+func main() {
+    mux := http.NewServeMux()
+
+    mux.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Christian Blanco - 202000173")
+    })
+
+    // Configurar CORS
+    c := cors.AllowAll()
+
+    // Iniciar el servidor en el puerto 8000 con CORS habilitado
+    handler := c.Handler(mux)
+    fmt.Println("Servidor escuchando en el puerto 8000...")
+    http.ListenAndServe(":8000", handler)
+}
---
+++ tareas/tarea1/frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [mensaje, setMensaje] = useState('');
+  const [mostrarDatos, setMostrarDatos] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      // Realizar una solicitud al servidor Go para obtener el mensaje
+      const response = await fetch('http://localhost:8000/data');
+      const data = await response.text();
+
+      // Actualizar el estado del mensaje
+      setMensaje(data);
+
+      // Alternar el estado de mostrarDatos
+      setMostrarDatos(!mostrarDatos);
+    } catch (error) {
+      console.error('Error al obtener el mensaje:', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className='btn-mostrar-ocultar' onClick={handleClick}>
+          {mostrarDatos ? 'Ocultar datos' : 'Mostrar datos'}
+        </button>
+        {mostrarDatos && <h1>{mensaje}</h1>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
